ef040d4a75c86366b4fad95b10baf957a7f2c804
hedye000 <133121319+hedye000@users.noreply.github.com>
2023-07-30 18:03:27 +0100

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x661fc58ac5a363deb88d70f3f4c7e02c68da14de";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
